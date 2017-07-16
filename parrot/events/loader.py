4b43ec9614930616224b1bd565114edbe8717c0a
root <root@oujianrong-hadoop-dev002-bjdx.qiyi.virtual>
2017-07-17 00:35:21 +0800

    fea --- add yaml loader class

--- parrot/events/loader.py
+++ parrot/events/loader.py
@@ -1,5 +1,23 @@
+import sys
+import os
 
 import yaml
 
+
 class Loader(object):
-    pass
+
+    def __init__(self, conf):
+        self.config_dir = conf['config_dir']
+        self.reload()
+
+    def reload(self):
+        files = os.listdir(self.config_dir)
+        event_books = dict()
+        for f in files:
+            event_books[f] = Loader.load(os.path.join(self.config_dir, f))
+        self.event_books = event_books
+
+    @staticmethod
+    def load(file_name):
+        with open(file_name, 'r') as src:
+            return yaml.load(src)
